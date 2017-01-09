2661466f02c03a2bd555ca4d4815d83bfabbd6b5
Bojie Li <bojieli@gmail.com>
2017-01-09 11:42:37 +0800

    DiseaseInfo table

---
+++ DiseaseInfo.py
@@ -0,0 +1,81 @@
+#/usr/bin/python
+# -*- coding:utf-8 -*-
+from scrapywrapper.wrapper import SpiderFactory
+from scrapywrapper.config import ScrapyWrapperConfig
+
+
+class ScrapyConfig(ScrapyWrapperConfig):
+	crawlera_enabled = True
+	begin_urls = ["http://www.phsciencedata.cn/Share/ky_sjml.jsp?id=8defcfc2-b9a4-4225-b92c-ebb002321cea&show=0"]
+	steps = {
+		"begin": {
+			'res': [{
+				'selector_css': 'div.biao',
+				'next_step': 'db_major_class'
+			},
+			{
+				'selector_css': 'div.con > p > a',
+				'next_step': 'db_class'
+			}]
+		},
+		"db_major_class": {
+			'type': "db",
+			'table_name': "DiseaseClassification",
+			'fields': [{
+				'name': 'DiseaseName',
+				'selector_xpath': '//h4/text()',
+				'required': True
+			}, {
+				'name': 'DiseasePath',
+				'selector_xpath': '/div/@id',
+				'required': True
+			}]
+		},
+		"db_class": {
+			'type': "db",
+			'table_name': "DiseaseClassification",
+			'fields': [{
+				'name': 'DiseaseName',
+				'selector_xpath': '//a',
+				'required': True
+			}, {
+				'name': 'DiseasePath',
+				'selector_xpath': '//a/@itemid',
+				'required': True
+			}],
+			'res': {
+				'selector_xpath': '//a/@href',
+				'selector_regex': '(ky_sjml.jsp\?id=.*)',
+				'next_step': 'content'
+			}
+		},
+		"content": {
+			'res': {
+				'selector_css': '.content_r',
+				'next_step': 'db'
+			}
+		},
+		"db": {
+			'type': "db",
+			'table_name': "DiseaseInfo",
+			'fields': [{
+				'name': "DiseaseID",
+				'selector': lambda _, meta: meta['$$info_id'],
+				'required': True
+			}, {
+				'name': "DiseaseDescription",
+				'selector_css': '#tabs-note',
+				'required': True,
+				'strip_tags': False
+			}, {
+				'name': "DiseaseName",
+				'value': None
+			}, {
+				'name': "DiseasePath",
+				'value': None
+			}]
+		}
+	}
+
+myspider = SpiderFactory(ScrapyConfig(), __name__)
+
