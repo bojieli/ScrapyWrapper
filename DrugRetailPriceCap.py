4d77d7cfb2ce5858843f924e6f524c32a44e784d
Bojie Li <bojieli@gmail.com>
2017-01-10 08:05:56 +0800

    fix unicode in DrugRetailPriceCap

--- DrugRetailPriceCap.py
+++ DrugRetailPriceCap.py
@@ -5,7 +5,7 @@ from scrapywrapper.config import ScrapyWrapperConfig
 
 class ScrapyConfig(ScrapyWrapperConfig):
 	def url_generator(self):
-		for i in range(769999,0,-1):
+		for i in range(769580,0,-1):
 			#yield 'http://db.yaozh.com/api/index.php/Home/index/yaopinjiage/id/' + str(i)
 			yield 'http://128.199.95.148:9574/yaopinjiage/id/' + str(i)
 
@@ -39,32 +39,32 @@ class ScrapyConfig(ScrapyWrapperConfig):
 				}
 			}, {
 				'name': "DrugName",
-				'selector_json': "药品名称",
+				'selector_json': u"药品名称",
 				'required': True
 			}, {
 				'name': "PriceSettingRegion",
-				'selector_json': "定价地区"
+				'selector_json': u"定价地区"
 			}, {
 				'name': "DosageType",
-				'selector_json': "剂型"
+				'selector_json': u"剂型"
 			}, {
 				'name': "Specification",
-				'selector_json': "规格"
+				'selector_json': u"规格"
 			}, {
 				'name': "UnitOfMeasurement",
-				'selector_json': "单位"
+				'selector_json': u"单位"
 			}, {
 				'name': "RetailPriceCap",
-				'selector_json': "最高零售价(元)"
+				'selector_json': u"最高零售价(元)"
 			}, {
 				'name': "DrugManufacturerName",
-				'selector_json': "生产企业"
+				'selector_json': u"生产企业"
 			}, {
 				'name': "CommentInfo",
-				'selector_json': "备注"
+				'selector_json': u"备注"
 			}, {
 				'name': "EffectiveDate",
-				'selector_json': "执行日期",
+				'selector_json': u"执行日期",
 			}, {
 				'name': "RegionID",
 				'reference': {
@@ -72,11 +72,11 @@ class ScrapyConfig(ScrapyWrapperConfig):
 					'field': "PriceSettingRegion",
 					'remote_field': "Name",
 					'remote_id_field': 'PID',
-					'match': 'prefix'
+					'match': 'lpm'
 				}
 			}, {
 				'name': "DocumentNumber",
-				'selector_json': "文件号"
+				'selector_json': u"文件号"
 			}]
 		}
 	}
