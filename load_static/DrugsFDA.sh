d41e7ad1eb8e7fab3e419b47f97af4430c757ccf
Bojie Li <bojieli@gmail.com>
2017-02-20 05:55:16 +0800

    load drug at FDA

---
+++ load_static/DrugsFDA.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+rm tmp_drugs -rf
+mkdir tmp_drugs
+cd tmp_drugs
+wget https://www.fda.gov/downloads/Drugs/InformationOnDrugs/UCM527389.zip
+unzip UCM527389.zip
+cd ..
+
+python load_tab.py tmp_drugs/Products.txt FdaProducts
+python load_tab.py tmp_drugs/Submissions.txt FdaSubmissions
+python load_tab.py tmp_drugs/SubmissionPropertyType.txt FdaSubmissionPropertyType
+python load_tab.py tmp_drugs/SubmissionClass_Lookup.txt FdaSubmissionClassLookup
+python load_tab.py tmp_drugs/TE.txt FdaTE
+python load_tab.py tmp_drugs/MarketingStatus_Lookup.txt FdaMarketingStatusLookup
+python load_tab.py tmp_drugs/MarketingStatus.txt FdaMarketingStatus
+python load_tab.py tmp_drugs/ApplicationsDocsType_Lookup.txt FdaApplicationsDocsTypeLookup
+python load_tab.py tmp_drugs/ActionTypes_Lookup.txt FdaActionTypesLookup
+python load_tab.py tmp_drugs/ApplicationDocs.txt FdaApplicationDocs
+python load_tab.py tmp_drugs/Applications.txt FdaApplications
---
+++ load_static/load_tab.py
@@ -0,0 +1,67 @@
+#coding=utf8
+import pymssql
+import sys
+import dateparser
+
+server = "114.215.255.52"
+user = "AddDataUser"
+password = "Dv*#d~18K"
+dbname = "DB_Medicine"
+
+conn = pymssql.connect(server, user, password, dbname, charset="utf8")
+cursor = conn.cursor()
+table_data = []
+f = open(sys.argv[1], 'r')
+table_name = sys.argv[2]
+
+def parse_date(s):
+	month_map = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
+	x = [v.strip(',') for v in s.split(' ')]
+	return x[2] + '-' + format(month_map[x[0]], '02d') + '-' + format(int(x[1]), '02d')
+
+fields = ['ID']
+value_types = ['NEWID()']
+table_data = []
+first_line = True
+count = 0
+for line in f:
+	values = line.split('\t')
+	values = [v.strip() for v in values]
+	if first_line:
+		first_line = False
+		if "ID" in values:
+			fields = values
+			value_types = ['%s' for v in values]
+		elif values[0].endswith('ID'):
+			values[0] = 'ID'
+			fields = values
+			value_types = ['%s' for v in values]
+		else:
+			fields.extend(values)
+			value_types.extend(['%s' for v in values])
+
+		fields = [s.replace('_','').replace(';','') for s in fields]
+		print(fields)
+		continue
+	count += 1
+	table_data.append([str(count)] + values)
+
+cursor.execute("DELETE FROM " + table_name)
+cursor.execute("SET ANSI_WARNINGS off")
+
+def insertmany(cursor, table_name, fields, value_types, table_data):
+	cursor.execute("INSERT INTO " + table_name + " (" + ",".join(fields) + ") VALUES "
+		+ ",".join([ "(" + ",".join(value_types) + ")" for row in table_data ]),
+		tuple([item for sublist in table_data for item in sublist]))
+
+def insertshard(conn, cursor, table_name, fields, value_types, table_data):
+	shard_size = 1000
+	total = len(table_data)
+	for i in range(0, total, shard_size):
+		shard = table_data[i:i+shard_size]
+		insertmany(cursor, table_name, fields, value_types, shard)
+		print(i)
+
+insertshard(conn, cursor, table_name, fields, value_types,  table_data)
+conn.commit()
+conn.close()
