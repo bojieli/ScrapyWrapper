9b97baa72e32ac86ff8eb9fe3a56425c87bece94
Bojie Li <bojieli@gmail.com>
2017-03-06 09:23:13 +0800

    RunSpider with pyc

--- RunSpider.sh
+++ RunSpider.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 cd $(dirname $0)
-SCRIPT=$1
-[ ! -f "${SCRIPT}.py" ] && echo "script ${SCRIPT}.py not found" && exit 1
+SCRIPT="$1"
+[ ! -f "${SCRIPT}.py" ] && [ ! -f "${SCRIPT}.pyc" ] && echo "Neither script ${SCRIPT}.py nor compiled script ${SCRIPT}.pyc was found" && exit 1
+if [ -f "${SCRIPT}.py" ]; then
+	SCRIPTFILE="${SCRIPT}.py"
+else
+	SCRIPTFILE="${SCRIPT}.pyc"
+fi
 LOGDIR=logs
 LOGSUBDIR=$SCRIPT
 mkdir -p $LOGDIR/$LOGSUBDIR
 LOGFILE=$LOGDIR/$LOGSUBDIR/$(date '+%Y-%m-%d').log
 export PATH=$PATH:/usr/local/bin
-/usr/local/bin/scrapy runspider ${SCRIPT}.py >$LOGFILE 2>&1
+/usr/local/bin/scrapy runspider ${SCRIPTFILE} >$LOGFILE 2>&1
 exit 0
